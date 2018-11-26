9e88f4b2a0001cd52c17d30a8f5c01005c0d66b1
qozle <mf.qozle@gmail.com>
2018-11-26 12:07:20 -0500

    tidied up the wp theme

--- blog/wp-content/themes/dctheme/sidebar.php
+++ blog/wp-content/themes/dctheme/sidebar.php
@@ -1,16 +1,16 @@
 <div class="col-sm-2 blog-sidebar">
           <div class="sidebar-module sidebar-module-inset">
-            <h6>About</h6>
+            <h6 class="blog-sidebar-header">About</h6>
             <p><?php the_author_meta( 'description' ); ?></p>
           </div>
           <div class="sidebar-module">
-            <h6>Archives</h6>
+            <h6 class="blog-sidebar-header">Archives</h6>
             <ol class="list-unstyled">
               <?php wp_get_archives( 'type=monthly' ); ?>
             </ol>
           </div>
           <div class="sidebar-module">
-            <h6>Elsewhere</h6>
+            <h6 class="blog-sidebar-header">Elsewhere</h6>
             <ol class="list-unstyled">
               <li><a href="#">GitHub</a></li>
               <li><a href="#">Twitter</a></li>
